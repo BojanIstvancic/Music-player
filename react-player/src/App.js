e0a7dd1216bd77a413ea2b40fc391d71da787c09
BojanIstvancic <bojan.istvancic@gmail.com>
2021-03-03 14:54:21 +0100

    UUId extension added, data added, SCSS styling updated

--- react-player/src/App.js
+++ react-player/src/App.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Player from "./Components/Player";
 import Song from "./Components/Song";
 import "./styles/app.scss";
+import data from "./data";
 
 function App() {
+  // state
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
   return (
     <div className="App">
-      <Song />
+      <Song currentSong={currentSong} />
       <Player />
     </div>
   );

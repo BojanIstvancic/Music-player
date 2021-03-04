4f81020610b55c933f91a7d1cbaaf303629242c7
BojanIstvancic <bojan.istvancic@gmail.com>
2021-03-04 13:14:12 +0100

    Add decoration to active song

--- react-player/src/Components/Library.js
+++ react-player/src/Components/Library.js
@@ -1,15 +1,18 @@
 import React from "react";
 import LibrarySong from "./LibrarySong";
 
-const Library = ({ songs, setCurrentSong, audioRef, isPlaying }) => {
+const Library = ({ songs, setCurrentSong, audioRef, isPlaying, setSongs }) => {
   return (
     <div className="library">
       <h2>Library</h2>
       <div className="library-songs">
         {songs.map((song) => (
           <LibrarySong
+            songs={songs}
+            setSongs={setSongs}
             song={song}
             key={song.id}
+            id={song.id}
             setCurrentSong={setCurrentSong}
             audioRef={audioRef}
             isPlaying={isPlaying}
--- react-player/src/Components/LibrarySong.js
+++ react-player/src/Components/LibrarySong.js
@@ -1,7 +1,31 @@
 import React from "react";
 
-const LibrarySong = ({ song, setCurrentSong, audioRef, isPlaying }) => {
+const LibrarySong = ({
+  songs,
+  song,
+  setCurrentSong,
+  audioRef,
+  isPlaying,
+  id,
+  setSongs,
+}) => {
   const songSelectHandler = () => {
+    // Add active state
+
+    const newSongs = songs.map((song) => {
+      if (song.id === id) {
+        return {
+          ...song,
+          active: true,
+        };
+      } else {
+        return {
+          ...song,
+          active: false,
+        };
+      }
+    });
+    setSongs(newSongs);
     setCurrentSong(song);
     if (isPlaying) {
       const playPromise = audioRef.current.play();
@@ -14,7 +38,10 @@ const LibrarySong = ({ song, setCurrentSong, audioRef, isPlaying }) => {
   };
 
   return (
-    <div onClick={songSelectHandler} className="library-song">
+    <div
+      onClick={songSelectHandler}
+      className={`library-song ${song.active ? "selected" : ""}`}
+    >
       <img src={song.cover} alt={song.name} />
       <div className="song-description">
         <h3>{song.name}</h3>
